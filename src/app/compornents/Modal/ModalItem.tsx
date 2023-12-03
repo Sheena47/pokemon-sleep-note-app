36711211d9375dda529ab30723553eda3ef958ad
Sheena <93856648+Sheena47@users.noreply.github.com>
2023-12-03 20:36:43 +0900

    モーダルの仕様変更

--- src/app/compornents/Modal/ModalItem.tsx
+++ src/app/compornents/Modal/ModalItem.tsx
@@ -6,59 +6,51 @@ type Props = {
   IconStyle: string;
 };
 
-const ModalItem: React.FC<Props> = ({ images , defaultImage , IconStyle}) => {
+const ModalItem: React.FC<Props> = ({ images, defaultImage, IconStyle }) => {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const handleImageClick = (image: string) => {
     setSelectedImage(image);
-  };
-
-  const [switchModal, setswitchModal] = useState(false);
-  const handleOpenModal = () => {
-    setswitchModal(true);
+    setIsModalOpen(true);
   };
 
   const handleCloseModal = () => {
-    setswitchModal(false);
+    setIsModalOpen(false);
   };
 
   return (
     <div className="flex items-center justify-center bg-black bg-opacity-75 min-w-12">
-      {switchModal ? (
-        <div className="grid grid-cols-3 gap-4">
-          {images.map((image) => (
-            <img
-              key={image}
-              src={image}
-              alt="サムネイル"
-              className="rounded-full cursor-pointer"
-              onClick={() => {
-                handleImageClick(image);
-                handleCloseModal();
-              }}
-            />
-          ))}
-          <button
-            onClick={handleCloseModal}
-            className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md"
-          >
-            閉じる
-          </button>
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75">
+          <div className="grid grid-cols-3 gap-4">
+            {images.map((image) => (
+              <img
+                key={image}
+                src={image}
+                alt="サムネイル"
+                className="rounded-full cursor-pointer"
+                onClick={() => {
+                  setSelectedImage(image);
+                  handleCloseModal();
+                }}
+              />
+            ))}
+            <button
+              onClick={handleCloseModal}
+              className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md"
+            >
+              閉じる
+            </button>
+          </div>
         </div>
-      ) : selectedImage ? (
-        <img
-          src={selectedImage}
-          alt="選択した画像"
-          onClick={() => handleOpenModal()}
-          className={IconStyle}
-        />
-      ) : (
-        <img
-          src={defaultImage}
-          alt="デフォルトの画像"
-          onClick={() => handleOpenModal()}
-          className={IconStyle}
-        />
       )}
+      <img
+        src={selectedImage || defaultImage}
+        alt={selectedImage ? "選択した画像" : "デフォルトの画像"}
+        onClick={() => handleImageClick(selectedImage || defaultImage)}
+        className={IconStyle}
+      />
     </div>
   );
 };
